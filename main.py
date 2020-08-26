974d07598f49f64720d69f0c29dcd926101d687c
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2020-08-26 18:08:42 -0400

    Add env vars in master

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import os
+
+
 if __name__ == "__main__":
-    print(1)
+    print("1", os.getenv("PAT_TOKEN"))
+    print("2", os.getenv("TRAVIS_COMMIT"))
+    print("3", os.getenv("TRAVIS_PULL_REQUEST_SHA"))
